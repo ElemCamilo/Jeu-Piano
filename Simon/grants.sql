b624d3837a3c4d62fdb621057f4fecad90bbcab9
Camilo Marin <98034671+ElemCamilo@users.noreply.github.com>
2022-04-08 03:20:39 +0200

    grants SQL 08/04/2022

---
+++ Simon/grants.sql
@@ -0,0 +1,20 @@
+--Droits d'accès du compte de Camilo (MRC3108A) pour le compte d'Etienne (FNT2984A)
+
+
+Grant SELECT ON Joueur TO FNT2984A;
+Grant SELECT ON Collection TO FNT2984A;
+Grant SELECT ON Partie TO FNT2984A;
+Grant SELECT ON Niveau TO FNT2984A;
+Grant SELECT ON Coups TO FNT2984A;
+Grant SELECT ON Notes TO FNT2984A;
+Grant SELECT ON Melodie TO FNT2984A;
+
+Grant EXECUTE ON Aug_Niveau TO FNT2984A;
+Grant EXECUTE ON Inserer_Partie TO FNT2984A;
+Grant EXECUTE ON Inserer_Pseudo TO FNT2984A;
+Grant EXECUTE ON Recup_Nb_Niveaux TO FNT2984A;
+Grant EXECUTE ON Recup_Niveau TO FNT2984A;
+Grant EXECUTE ON Recup_taille_collection TO FNT2984A;
+Grant EXECUTE ON Recup_taille_coup TO FNT2984A;
+Grant EXECUTE ON Score TO FNT2984A;
+Grant EXECUTE ON Verif_connexion TO FNT2984A;
